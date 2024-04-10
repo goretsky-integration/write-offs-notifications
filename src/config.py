1e081958c2d5fa49360929331bc4afdc459be27e
Eldos <eldos.baktybekov@gmail.com>
2024-04-10 11:49:12 +0600

    :wrench: Add config parser and config example file

---
+++ src/config.py
@@ -0,0 +1,33 @@
+import pathlib
+import tomllib
+from dataclasses import dataclass
+from zoneinfo import ZoneInfo
+
+__all__ = ('Config', 'load_config',)
+
+
+@dataclass(frozen=True, slots=True)
+class Config:
+    google_sheets_credentials_file_path: pathlib.Path
+    spreadsheet_key: str
+    timezone: ZoneInfo
+    units_storage_base_url: str
+
+
+def load_config(file_path: pathlib.Path) -> Config:
+    config_text = file_path.read_text(encoding='utf-8')
+    config = tomllib.loads(config_text)
+
+    google_sheets_credentials_file_path = (
+        pathlib.Path(config['google_sheets']['credentials_file_path'])
+    )
+    spreadsheet_key = config['google_sheets']['spreadsheet_key']
+    timezone = ZoneInfo(config['timezone'])
+    units_storage_base_url = config['units_storage']['base_url']
+
+    return Config(
+        google_sheets_credentials_file_path=google_sheets_credentials_file_path,
+        spreadsheet_key=spreadsheet_key,
+        timezone=timezone,
+        units_storage_base_url=units_storage_base_url,
+    )
