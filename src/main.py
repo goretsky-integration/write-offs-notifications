939d2da473a408b8760a50a6bb9d216139a3fa29
Eldos <eldos.baktybekov@gmail.com>
2024-04-13 23:04:37 +0600

    :sparkles: Add upcoming write-offs cells background color formatting

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import asyncio
 import datetime
+import logging
 import pathlib
 
 import gspread
 
+from colors import WRITE_OFF_TYPE_TO_COLOR
 from config import load_config
-from google_sheets import SpreadsheetContext
+from google_sheets import SpreadsheetContext, WorksheetContext
 from message_queue import publish_events
 from parsers import parse_worksheets_values, serialize_upcoming_write_offs
 from units_storage import get_units
+
+logger = logging.getLogger(__name__)
 
 
 async def main() -> None:
@@ -28,18 +32,34 @@ async def main() -> None:
     )
     value_ranges = spreadsheet_context.get_values(now)
 
-    worksheets = parse_worksheets_values(value_ranges, config.timezone)
+    write_offs = parse_worksheets_values(value_ranges, config.timezone)
 
     events = serialize_upcoming_write_offs(
-        worksheets=worksheets,
+        write_offs=write_offs,
         now=now,
         unit_name_to_id={unit.name: unit.id for unit in units},
     )
+
+    if not events:
+        logger.info('No events')
+        return
 
     await publish_events(
         message_queue_url=config.message_queue_url,
         events=events
     )
+
+    for event in events:
+        worksheet = spreadsheet_context.get_worksheet_by_title(
+            title=event.payload.unit_name,
+        )
+        background_color = WRITE_OFF_TYPE_TO_COLOR[event.payload.type]
+        worksheet_context = WorksheetContext(worksheet)
+
+        worksheet_context.update_cell_color(
+            cell_coordinates=event.payload.write_off_time_a1_coordinates,
+            background_color=background_color,
+        )
 
 
 if __name__ == '__main__':
